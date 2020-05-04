31970e1d3a0f2b8e5942f3d0907cb54eb37d9b2f
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-05-04 15:49:25 -0300

    Update submarino.py

--- submarino.py
+++ submarino.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
 from time import sleep
 
 browser = webdriver.Firefox()
@@ -21,15 +22,21 @@ while i < len(categoria):
 
 print("\n")
 
-escolha1 = int(input("Digite aqui a categoria escolhida: "))
+sleep(5)
 
-botao1 = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/ul/li"+str([escolha1]))
+escolha = int(input("Digite aqui a categoria escolhida: "))
+
+sleep(5)
+
+botao1 = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/ul/li"+str([escolha]))
 
 botao1.click()
 
+sleep(5)
+
 print("\n")
 
-print("***LIVROS DA CATEGORIA " + (categoria[escolha1-1].text).upper() + "***\n")
+print("***LIVROS DA CATEGORIA " + (categoria[escolha-1].text).upper() + "***\n")
 
 sleep(10)
 
@@ -41,7 +48,45 @@ livros = browser.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/
 
 precos = browser.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/a/section/div/div/div/span")
 
-for livro in livros and preco in precos:
+i = 0
+
+for livro in livros:	
+
+	print(str(livro.text) + "-" + str(precos[i].text))
+
+	i = i + 1
+"""
+i = 0
+
+print("\n")
+print("***SUBCATEGORIAS***\n")
+
+subcategorias = browser.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/ul/li/a/span")
+
+while i < len(subcategorias):
+
+	print(str(i+1) + "-" + subcategorias[i].text)
+
+	i = i + 1
+
+print("\n0-Pŕoxima página\n")
+
+escolha2 = int(input("Digite a sua escolha aqui: "))
+
+while escolha2 == 0:
 	
-	print(livro.text + preco.text)
+	botao2 = browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/div/div[1]/div/div[2]/div[4]/div/div/div/div[2]/div/ul/li[10]/a")
+
+	botao2.click()
+
+	sleep(3)
+
+	escolha2 = int(input("Digite a sua escolha aqui: "))
+else:
+
+	botao2 = browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[2]/div/aside/div[1]/div[2]/div/div/div/span/div/div/section[1]/div/ul/li"+str([escolha2])+"/a/span")
+
+botao2.click()"""
+
+
 
