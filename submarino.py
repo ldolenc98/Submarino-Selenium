2681f6eeb1521f75d78fa65349e66ffbe14e9cb2
Luciano Nereo Dolenc <62342413+ldolenc98@users.noreply.github.com>
2020-05-13 09:39:10 -0300

    Update submarino.py

--- submarino.py
+++ submarino.py
@@ -20,56 +20,51 @@ sleep(10)
 
 while i < len(categorias):
 
-	sleep(10)
+	try:
+
+		teste = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/header/h2")
+
+	except:
+
+		browser.refresh()
+
+		sleep(30)
+
+	sleep(30)
 
 	categoria = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/ul/li" + str([i+1]) + "/a/span")
+
+	nome_categoria = categoria.text.upper()
 
 	sleep(2)
 
 	categoria.click()
 
-	j = 21
+	j = 0
 
-	sleep(40)
+	sleep(10)
+
+	try:
+
+		teste1 = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/header/h2")
+
+	except:
+
+		browser.refresh()
+
+		sleep(15)
 	
 	subcategorias_teste = WebDriverWait(browser,300).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[2]/div/aside/div[1]/div[2]/div/div/div/span/div/div/section[1]/div/ul/li/a/span")))
 
 	subcategorias = browser.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/ul/li/a/span")
 
 	sleep(15)
+
+	z = 1
 	
-	try:
-		ver_todos = browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[2]/div/aside/div[1]/div[2]/div/div/div/span/div/div/section[1]/div/button")
-
-		ver_todos.click()
-
-		sleep(5)
-
-	except:
-
-		""
-
-	subcategoria_plus = browser.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/section/div/ul/li/a/span")
-
-	sleep(1)
-
-	try:
-
-		ver_todos.click()
-
-	except:
-
-		""
-
-	a = len(subcategorias) + len(subcategoria_plus)
-
-	print(a)
-
-	while j < a:
+	while j < len(subcategorias):
 
 		sleep(3)
-
-		z = 1
 	
 		if j > 19:
 
@@ -79,9 +74,11 @@ while i < len(categorias):
 
 			sleep(5)
 
-			subcategoria_plus = browser.find_elements_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/section/div/ul/li/a/span")
+			subcategoria_plus = browser.find_element_by_xpath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[2]/div/aside/div[1]/div[2]/div/div/div/span/div/div/section[1]/div/section/div/ul/li" + str([z]) + "/a/span")
 
-			subcategoria_plus[z].click()
+			nome_subcategoria = subcategoria_plus.text.upper()
+
+			subcategoria_plus.click()
 
 			z = z + 1		
 
@@ -93,9 +90,22 @@ while i < len(categorias):
 
 			subcategoria = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/span/div/div/section/div/ul/li"+str([j+1])+"/a/span")
 
+			nome_subcategoria = subcategoria.text.upper()
+
 			subcategoria.click()
 
 		sleep(60)
+
+		try:
+
+			teste2 = browser.find_element_by_xpath("/html/body/div/div/div/div/div/div/div/div/div/aside/div/div/div/div/div/div/section/header/h2")
+
+		except:
+
+			browser.refresh()
+
+			sleep(30)
+
 
 		livros = WebDriverWait(browser,60).until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/a/section/div/div/h2")))
 
@@ -113,9 +123,9 @@ while i < len(categorias):
 
 			if precos[l].text == "" :
 				
-				arquivo.write(str(str(livro.text) + "| Produto fora de estoque\n"))
+				arquivo.write(str(str(nome_categoria.upper())+ "|" + str(nome_subcategoria.upper()) + "|" + str(livro.text) + "| Produto fora de estoque\n"))
 
-				print(str(livro.text) + "| Produto fora de estoque")
+				print(str(nome_categoria.upper())+ "|" + str(nome_subcategoria.upper()) + "|" + str(livro.text) + "| Produto fora de estoque")
 
 				l = l + 1
 
@@ -124,9 +134,9 @@ while i < len(categorias):
 				
 			elif precos[l+1].text == "Inclui oferta":
 
-				arquivo.write(str(str(livro.text) + "|" + str(precos[l].text + "\n")))	
+				arquivo.write(str(str(nome_categoria.upper())+ "|" + str(nome_subcategoria.upper()) + "|" + str(livro.text) + "|" + str(precos[l].text + "\n")))	
 
-				print(str(livro.text) + "|" + str(precos[l].text))
+				print(str(nome_categoria.upper())+ "|" + str(nome_subcategoria.upper()) + "|" + str(livro.text) + "|" + str(precos[l].text))
 
 				l = l + 4
 
@@ -135,9 +145,9 @@ while i < len(categorias):
 				
 			else:
 
-				arquivo.write(str(str(livro.text) + "|" + str(precos[l].text + "\n")))	
+				arquivo.write(str(str(nome_categoria.upper())+ "|" + str(nome_subcategoria.upper()) + "|" + str(livro.text) + "|" + str(precos[l].text + "\n")))	
 
-				print(str(livro.text) + "|" + str(precos[l].text))
+				print(str(nome_categoria.upper())+ "|" + str(nome_subcategoria.upper()) + "|" + str(livro.text) + "|" + str(precos[l].text))
 
 				try:
 				
